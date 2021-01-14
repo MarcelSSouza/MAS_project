f5d37056a3839a404507ecee3a7b4befbcd5b118
MarcelSSouza <marcelaraujosantossouza19@gmail.com>
2021-01-14 18:05:34 +0000

    Padrao de projeto criado. Uma function para cadagit add .

---
+++ index.js
@@ -0,0 +1,20 @@
+function menu_replace(number) {
+    var menu = document.getElementById('menu')
+    menu.className = 'd-none'
+    menu.className('display')
+    switch (number) {
+        case '1':
+            break;
+        case '2':
+            break;
+        case '3':
+            break;
+        case '4':
+            break;
+        case '5':
+            break;
+        case '6':
+            break;
+    }
+
+}
